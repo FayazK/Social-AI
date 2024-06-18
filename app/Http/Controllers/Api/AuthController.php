0c66d7bd0cf236463d561433184f8a744228a068
Fayaz K <info@fayazk.com>
2024-06-18 16:35:55 +0500

    ✨ (AuthController.php): add AuthController for API authentication ✨ (ApiLoginRequest.php): add ApiLoginRequest for handling login validation ✨ (ApiAuthResource.php): add ApiAuthResource for formatting auth responses ✨ (User.php): add HasApiTokens trait to User model for API token management

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\ApiLoginRequest;
+use App\Http\Resources\ApiAuthResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ *
+ */
+class AuthController extends Controller
+{
+
+    /**
+     * @param ApiLoginRequest $request
+     * @return ApiAuthResource
+     */
+    public function login(ApiLoginRequest $request)
+    {
+        $request->authenticate();
+        $user = $request->user();
+        $token = $user->createToken('app')->plainTextToken;
+
+        return ApiAuthResource::make(['user' => $user, 'token' => $token]);
+
+    }// login
+
+}// AuthController
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+//use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
+/**
+ *
+ * @mixin Builder
+ */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
